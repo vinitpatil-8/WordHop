a9995a94fd9819dc9ebe4fd31736d6577e5ff29f
Vinit <vinit.2007.20@gmail.com>
2025-09-09 11:19:50 +0530

    Initial Commit: Word Hop

---
+++ src/wordhop.js
@@ -0,0 +1,13 @@
+const dateHolder = document.getElementById("dateHolder");
+
+const d = new Date();
+let monthnum = d.getMonth();
+let monthlist = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let month = monthlist[monthnum]
+let day = d.getDate();
+let year = d.getFullYear();
+let fullDate = `${month} ${day}, ${year}`
+dateHolder.innerHTML = fullDate
+
+
+let word = "fives"
